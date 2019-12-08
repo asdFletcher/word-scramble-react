96d8838831beb5794bab1743bc12edd4ad5457a7
Fletcher LaRue <laruefletcher@gmail.com>
2019-12-08 02:25:21 -0800

    attempts export as npm package

--- word-scramble/README.md
+++ word-scramble/README.md
@@ -1,3 +1,17 @@
+### This repo is now designed as an npm package
+
+The npm package looks for exports in the `index.js` file located in `src/index.js`
+
+The format of this is:
+
+
+
+
+
+
+
+---
+
 
 .env file contents for deployed backend:
 REACT_APP_BACKEND_BASE_URL=https://ocean-commotion-backend.herokuapp.com
@@ -7,5 +21,7 @@ REACT_APP_BACKEND_BASE_URL=http://localhost:3001
 
 Note: Remember to restart the app when the .env file is changed
 
-    "start": "react-scripts start",
-    "build": "react-scripts build",
+
+`Compile the entire src directory and output it to the lib directory by using either --out-dir or -d. This doesn't overwrite any other files or directories in lib.`
+Source: https://babeljs.io/docs/en/babel-cli
+
--- word-scramble/src/index.js
+++ word-scramble/src/index.js
@@ -1,45 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Nav from "./components/Nav.js";
-import Title from "./components/Title.js";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/index.js';
-
-import HowToPlay from "./components/HowToPlay.js";
-import GameComponent from "./components/GameComponent.js";
-import HiScores from "./components/HiScores.js";
-import AboutUs from "./components/AboutUs.js";
-
-import "./styles/reset.css";
-import "./styles/styles.scss";
-
-class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Title />
-            <div className="nav-and-content-container">
-              <Nav />
-              <Switch>
-                <Route exact path="/how-to-play" component={HowToPlay} />
-                <Route exact path="/play-game" component={GameComponent} />
-                <Route exact path="/hi-scores" component={HiScores} />
-                <Route exact path="/about-us" component={AboutUs} />
-                <Route path="/" component={GameComponent} />
-              </Switch>
-            </div>
-          </div>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-}
+import App from './app';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-// export default App;
-
